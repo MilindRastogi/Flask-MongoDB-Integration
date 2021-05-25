ed29c7e963a393624f4f0662630aa12d3fa210e0
Milind Rastogi <milind.189303058@muj.manipal.edu>
2021-05-25 10:30:12 +0530

    created flask form

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
+
+app = Flask("myapp")
+app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/mydb"
+mongo = PyMongo(app)
+customer_collection = mongo.db.customers
+
+
+@app.route("/milind")
+def home_page():
+    customer = (customer_collection.find())
+    print(customer)
+    return render_template('index.html', customer=customer)
+
+
+@app.route("/form")
+def form():
+
+    return render_template('form.html')
+
+
+@app.route("/data", methods=['GET'])
+def show_data():
+    if request.method == 'GET':
+        name = request.args.get("x")
+        phone = request.args.get("y")
+        loc = request.args.get("z")
+        if name != "" and phone != "" and loc != "":
+            customer = customer_collection.insert_one(
+                {"name": name, "phone": phone, "location": loc})
+            return ("data added to the database")
+        else:
+            return ("Kindly fill the form")
+
+
+@app.route("/delete")
+def delete():
+    customer_collection.remove({})
+    return "All data deleted"
+
+
+app.run(debug=True)
